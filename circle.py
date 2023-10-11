220fe4b33b9bfb78ebc6cb8f86c51aaf52e74105
julia <z3541706@yandex.ru>
2023-10-11 16:10:30 +0300

    .

--- circle.py
+++ circle.py
@@ -2,9 +2,16 @@ import math
 
 
 def area(r):
+    """
+    функция area возвращает площадь круга по заданному радиусу
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """
+    функция perimetr возвращает периметр по заданному радиусу
+    """
     return 2 * math.pi * r
+
 
--- triangle.py
+++ triangle.py
@@ -1,4 +1,10 @@
 def area(a, h):
+    """
+    функция area возвращает площадь треугольника
+    """
     return a * h / 2
 def perimeter(a, b, c):
+    """
+    функция perimetr возвращает периметр треугольника
+    """
     return a + b + c
